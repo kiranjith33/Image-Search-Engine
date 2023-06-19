91028cc9cb5ee5e6c0a608ef151c0be59bbea163
kiranjith <58938818+kiranjith33@users.noreply.github.com>
2023-06-20 03:24:18 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 
+     // Creating the Variables
 
 const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result")
 const showMoreBtn = document.getElementById("show-more-btn")
 
+        //    Unsplash API Key
+
  const accessKey = "vuxWfvG_NrEc1bdYMnJmPNzmiCx-4CKLz1jNZvRpn8s";
 
 let keyword = "";
 let page = 1;
 
+    // Async Function For Calling the API
+
 async function searchImages(){
     keyword = searchBox.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=30`;
      const response = await fetch(url);
      const data = await response.json();
       
+
+
       if(page === 1){
         searchResult.innerHTML = "";
       }
@@ -32,11 +39,13 @@ async function searchImages(){
         searchResult.appendChild(imageLink);
         console.log(searchResult);
      })
-
-     showMoreBtn.style.display = "block";
+        //  For Show More Button
+    
+        showMoreBtn.style.display = "block";
 
 
 }
+          // Adding the Click Function
 searchForm.addEventListener("submit", (e) => {
     e.preventDefault();
     page = 1;
